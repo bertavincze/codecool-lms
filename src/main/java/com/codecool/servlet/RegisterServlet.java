ab728bcc56b7cc3c94e629feb5ecf2070cac3339
AndreaSzoboszlai <szobandi@gmail.com>
2019-03-11 14:19:09 +0100

    started servlet for registring new user

---
+++ src/main/java/com/codecool/servlet/RegisterServlet.java
@@ -0,0 +1,23 @@
+package com.codecool.servlet;
+
+import com.codecool.model.Greeting;
+import com.codecool.service.GreetingService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+    private final GreetingService service = new GreetingService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+}
