adb3d016853dd679bcd1e53417bf0f3fdfe4910d
berta <berta.vincze@gmail.com>
2019-03-11 19:53:21 +0100

    Remove isPublished field as it will be inherited from superclass

--- src/main/java/com/codecool/model/curriculum/AssignmentPage.java
+++ src/main/java/com/codecool/model/curriculum/AssignmentPage.java
@@ -4,7 +4,6 @@ public class AssignmentPage extends Page {
 
     private String question;
     private int maxScore;
-    private boolean isPublished;
 
     public AssignmentPage(String title, String question, int maxScore) {
         super(title);
