48f98c7a3c94e458124d8a61a300830e8b0be369
berta <berta.vincze@gmail.com>
2019-03-11 20:12:50 +0100

    Add solution field (String for now), add getters and setter for solution

--- src/main/java/com/codecool/model/curriculum/AssignmentPage.java
+++ src/main/java/com/codecool/model/curriculum/AssignmentPage.java
@@ -4,6 +4,7 @@ public class AssignmentPage extends Page {
 
     private String question;
     private int maxScore;
+    private String solution;
 
     public AssignmentPage(String title, String question, int maxScore) {
         super(title);
@@ -11,4 +12,19 @@ public class AssignmentPage extends Page {
         this.maxScore = maxScore;
     }
 
+    public String getQuestion() {
+        return question;
+    }
+
+    public int getMaxScore() {
+        return maxScore;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
 }
