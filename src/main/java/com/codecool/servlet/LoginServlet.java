a9b7b5e889d839fa255a0753ef49c165fb7ad9a7
berta <berta.vincze@gmail.com>
2019-03-11 19:21:43 +0100

    Update LoginServlet to give each user a new session

--- src/main/java/com/codecool/servlet/LoginServlet.java
+++ src/main/java/com/codecool/servlet/LoginServlet.java
@@ -1,18 +1,40 @@
 package com.codecool.servlet;
 
+import com.codecool.model.user.User;
+import com.codecool.model.user.UserRole;
+import com.codecool.service.UserList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String password = req.getParameter("password");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
+        User currentUser = null;
+        for (User user : UserList.getInstance().getUsers()) {
+            if (name.equals(user.getName()) && password.equals(user.getPassword())) {
+                currentUser = user;
+                HttpSession session = request.getSession();
+                session.setAttribute("user", currentUser);
+            }
+        }
+
+        if (currentUser.getUserRole().equals(UserRole.MENTOR)) {
+            request.getRequestDispatcher("mentor.html").forward(request, response);
+        } else if (currentUser.getUserRole().equals(UserRole.STUDENT)) {
+            request.getRequestDispatcher("student.html").forward(request, response);
+        }
     }
 }
