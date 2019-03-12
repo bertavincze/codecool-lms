0feaaf5096fadb6e0bbc4e469419c9a426b895b8
AndreaSzoboszlai <szobandi@gmail.com>
2019-03-12 13:47:01 +0100

    minimal changes in jstl code due to user class modification

--- src/main/java/com/codecool/model/user/Student.java
+++ src/main/java/com/codecool/model/user/Student.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class Student extends User {
 
-    private List<Solution> solutionList;
+    //private List<Solution> solutionList;
     private List<String> urlList;
 
     public Student(String name, String email, String password) {
         super(name, email, password);
-        solutionList = new ArrayList<>();
+        //solutionList = new ArrayList<>();
         urlList = new ArrayList<>(Arrays.asList("student.html", "curriculum-student.html", "stats.html"));
         // URLs accessible by Student user. If you make a new html page, add it here.
     }
 
-    public void addSolution(Solution solution) {
-        solutionList.add(solution);
-    }
+    //public void addSolution(Solution solution) {
+    //    solutionList.add(solution);
+    //}
 }
