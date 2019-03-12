fb8d600090f16d8b0662a73da22c5b0c2c948742
berta <berta.vincze@gmail.com>
2019-03-12 12:57:18 +0100

    Add UrlList instances

--- src/main/java/com/codecool/model/user/Mentor.java
+++ src/main/java/com/codecool/model/user/Mentor.java
@@ -1,8 +1,16 @@
 package com.codecool.model.user;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Mentor extends User {
+
+    private List<String> urlList;
 
     public Mentor(String name, String email, String password) {
         super(name, email, password);
+        urlList = new ArrayList<>(Arrays.asList("mentor.html", "curriculum-mentor.html", "submissions.html"));
+        // URLs accessible by Mentor user. If you make a new html page, add it here.
     }
 }
--- src/main/java/com/codecool/model/user/Student.java
+++ src/main/java/com/codecool/model/user/Student.java
@@ -1,14 +1,19 @@
 package com.codecool.model.user;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Student extends User {
 
     private List<Solution> solutionList;
+    private List<String> urlList;
 
     public Student(String name, String email, String password) {
         super(name, email, password);
         solutionList = new ArrayList<>();
+        urlList = new ArrayList<>(Arrays.asList("student.html", "curriculum-student.html", "stats.html"));
+        // URLs accessible by Student user. If you make a new html page, add it here.
     }
 
     public void addSolution(Solution solution) {
