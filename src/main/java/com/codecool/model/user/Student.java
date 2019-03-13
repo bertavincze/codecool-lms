c8402ffd2d2059c56b36b07c85806fb64cf6fcfb
berta <berta.vincze@gmail.com>
2019-03-13 22:32:40 +0100

    Remove urlList

--- src/main/java/com/codecool/model/user/Student.java
+++ src/main/java/com/codecool/model/user/Student.java
@@ -4,19 +4,15 @@ import com.codecool.model.curriculum.Solution;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Student extends User implements Serializable {
 
     private List<Solution> solutionList;
-    private List<String> urlList;
 
     public Student(String name, String email, String password) {
         super(name, email, password);
         solutionList = new ArrayList<>();
-        urlList = new ArrayList<>(Arrays.asList("student.html", "curriculum-student.html", "stats.html"));
-        //URLs accessible by Student user. If you make a new html page, add it here.
     }
 
     public void addSolution(Solution solution) {
@@ -27,10 +23,6 @@ public class Student extends User implements Serializable {
 
     public List<Solution> getSolutionList() {
         return solutionList;
-    }
-
-    public List<String> getUrlList() {
-        return urlList;
     }
 }
 
