55899921d1d422bc71f921891f7ff2560e124be3
NagyTamy <nagy.tamy@gmail.com>
2019-03-14 07:52:37 +0100

    Set up and redirect unique 404 page

---
+++ src/main/java/com/codecool/servlet/ErrorPageServlet.java
@@ -0,0 +1,45 @@
+package com.codecool.servlet;
+
+import com.codecool.database.UserList;
+import com.codecool.model.user.Mentor;
+import com.codecool.model.user.Student;
+import com.codecool.model.user.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/errorPage")
+public class ErrorPageServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        authUser(request, response);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+
+    private void authUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        User currentUser = (User) session.getAttribute("user");
+        if (currentUser != null) {
+            if (currentUser instanceof Mentor) {
+                request.getRequestDispatcher("mentor.html").forward(request, response);
+            } else if (currentUser instanceof Student) {
+                request.getRequestDispatcher("student.html").forward(request, response);
+            }
+        } else {
+            request.getRequestDispatcher("index.html").forward(request, response);
+        }
+
+    }
+
+}
