8cb2c3c113d463a687d2fbe236a0d77800c785b6
berta <berta.vincze@gmail.com>
2019-03-26 09:17:31 +0100

    Fix imports

--- src/main/java/com/codecool/servlet/GradingServlet.java
+++ src/main/java/com/codecool/servlet/GradingServlet.java
@@ -1,6 +1,6 @@
 package com.codecool.servlet;
 
-import com.codecool.database.PageList;
+import com.codecool.dao.database.PageList;
 import com.codecool.model.curriculum.AssignmentPage;
 import com.codecool.model.curriculum.Page;
 import com.codecool.model.curriculum.Solution;
--- src/main/java/com/codecool/servlet/PageHandlerServlet.java
+++ src/main/java/com/codecool/servlet/PageHandlerServlet.java
@@ -1,12 +1,7 @@
 package com.codecool.servlet;
 
-
-<<<<<<< HEAD
-import com.codecool.database.PageList;
-import com.codecool.database.UserList;
-=======
 import com.codecool.dao.database.PageList;
->>>>>>> Andi
+import com.codecool.dao.database.UserList;
 import com.codecool.model.curriculum.AssignmentPage;
 import com.codecool.model.curriculum.Page;
 import com.codecool.model.curriculum.Solution;
