68bf0cd08bd72456c514e3c28a872b1b82cf249b
berta <berta.vincze@gmail.com>
2019-03-27 14:47:02 +0100

    Add PageService class

---
+++ src/main/java/com/codecool/service/PageService.java
@@ -0,0 +1,23 @@
+package com.codecool.service;
+
+import com.codecool.dao.database.DatabasePageDao;
+import com.codecool.model.curriculum.Page;
+
+import java.sql.SQLException;
+
+public class PageService {
+
+    private final DatabasePageDao pageDao;
+
+    public PageService(DatabasePageDao pageDao) {
+        this.pageDao = pageDao;
+    }
+
+    public void addPage(Page page) throws SQLException {
+        try {
+            pageDao.addPage(page);
+        } catch (NumberFormatException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
