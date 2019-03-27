d89fa9437b79b61e6d3a679ffee96893f342ed38
berta <berta.vincze@gmail.com>
2019-03-27 19:46:20 +0100

    Fix constructor visibility modifier

--- src/main/java/com/codecool/dao/database/DatabasePageDao.java
+++ src/main/java/com/codecool/dao/database/DatabasePageDao.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 
 public class DatabasePageDao extends AbstractDao {
 
-    DatabasePageDao(Connection connection) {
+    public DatabasePageDao(Connection connection) {
         super(connection);
     }
 
