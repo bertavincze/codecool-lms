51adcbc7f380749ba10ef7f9153c4f29e097025c
AndreaSzoboszlai <szobandi@gmail.com>
2019-03-28 13:33:42 +0100

    Added DatabaseDao class

---
+++ src/main/java/com/codecool/dao/database/DatabaseAttendanceDao.java
@@ -0,0 +1,12 @@
+package com.codecool.dao.database;
+
+import java.sql.Connection;
+
+public class DatabaseAttendanceDao extends AbstractDao {
+
+    public DatabaseAttendanceDao(Connection connection) {
+        super(connection);
+    }
+
+
+}
